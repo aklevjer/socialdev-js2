10a1c4e373fd3488e6ca64ce308ca4f18a7dc7ba
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-04-18 21:59:53 +0200

    Added auth page logic

--- src/js/pages/auth.mjs
+++ src/js/pages/auth.mjs
@@ -1,3 +1,18 @@
+import { isLoggedIn } from "../utils/auth/index.mjs";
+import {
+  setLoginListener,
+  setRegisterListener,
+} from "../handlers/auth/index.mjs";
+
 export function authPage(path) {
-  // Auth page logic
+  if (isLoggedIn()) {
+    location.href = "/profile/";
+    return;
+  }
+
+  if (path === "/" || path === "/index.html") {
+    setLoginListener();
+  } else {
+    setRegisterListener();
+  }
 }
