7bc4ee8b4c17f238efe9bc7dc901270c1a14d034
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-04-18 22:01:35 +0200

    Implemented UI Listeners

--- src/js/utils/auth/checkAuth.mjs
+++ src/js/utils/auth/checkAuth.mjs
@@ -1,7 +1,9 @@
 import { isLoggedIn } from "./index.mjs";
+import { setUIListeners } from "../../handlers/ui/common.mjs";
 
 export function checkAuth(callback) {
   if (isLoggedIn()) {
+    setUIListeners();
     callback();
   } else {
     location.href = "/";
