0d3ebaa04670e9bbbec97400c46feb869f4580f0
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-04-19 23:28:07 +0200

    Updated an import for consistency

--- src/js/utils/auth/checkAuth.mjs
+++ src/js/utils/auth/checkAuth.mjs
@@ -1,5 +1,5 @@
 import { isLoggedIn } from "./index.mjs";
-import { setUIListeners } from "../../handlers/ui/common.mjs";
+import { setUIListeners } from "../../handlers/ui/index.mjs";
 
 export function checkAuth(callback) {
   if (isLoggedIn()) {
