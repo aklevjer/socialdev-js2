cb4be869efbf902acd1b52c73e9ed0ee9d0fa588
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-04-24 19:58:42 +0200

    Added delete and edit post functionality

--- src/js/api/posts/update.mjs
+++ src/js/api/posts/update.mjs
@@ -7,9 +7,14 @@ export async function updatePost(postId, postData) {
     body: JSON.stringify(postData),
   });
 
+  const responseData = await response.json();
+
   if (response.ok) {
-    return await response.json();
+    return responseData;
   }
 
-  throw new Error(`Failed to update the post with the id: ${postId}`);
+  const errorMessage =
+    responseData?.errors[0]?.message ||
+    `Failed to update the post with the id: ${postId}`;
+  throw new Error(errorMessage);
 }
