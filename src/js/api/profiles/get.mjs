5e196ee0be5fe360ecf18f15c0618022920b6142
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-04-26 19:16:48 +0200

    Added get profile posts functionality

--- src/js/api/profiles/get.mjs
+++ src/js/api/profiles/get.mjs
@@ -1,12 +1,38 @@
 import { authFetch } from "../index.mjs";
-import { API_PROFILES_URL } from "../../constants/index.mjs";
+import {
+  API_PROFILES_URL,
+  API_POSTS,
+  API_PARAMS_POSTS,
+} from "../../constants/index.mjs";
 
 export async function getProfileByName(name) {
   const response = await authFetch(`${API_PROFILES_URL}/${name}`);
 
+  const responseData = await response.json();
+
   if (response.ok) {
-    return await response.json();
+    return responseData;
   }
 
-  throw new Error(`Failed to get the profile with the name: ${name}`);
+  const errorMessage =
+    responseData?.errors[0]?.message ||
+    `Failed to get the profile with the name: ${name}`;
+  throw new Error(errorMessage);
+}
+
+export async function getPostsByProfile(name) {
+  const response = await authFetch(
+    `${API_PROFILES_URL}/${name}${API_POSTS}${API_PARAMS_POSTS}`,
+  );
+
+  const responseData = await response.json();
+
+  if (response.ok) {
+    return responseData;
+  }
+
+  const errorMessage =
+    responseData?.errors[0]?.message ||
+    `Failed to get posts for profile with the name: ${name}`;
+  throw new Error(errorMessage);
 }
