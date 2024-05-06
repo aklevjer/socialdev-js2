cc393d3b844edade4bfa859544b1b4814be38b9a
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-05-06 22:07:21 +0200

    Refactored alert to support toast notify

--- src/js/handlers/ui/alert.mjs
+++ src/js/handlers/ui/alert.mjs
@@ -1,10 +1,18 @@
 import { createAlertTemplate } from "../../templates/ui/index.mjs";
 import { clearElement } from "../../utils/html/index.mjs";
 
-export function showAlert(alertType, alertMessage, parentElement) {
-  const alertClone = createAlertTemplate(alertType, alertMessage);
+export function showAlert(alertType, alertMessage, parentElement = null) {
+  const isToast = parentElement === null;
+  const { alertClone, alertContainer } = createAlertTemplate(
+    alertType,
+    alertMessage,
+    isToast,
+  );
 
-  if (parentElement) {
+  if (isToast) {
+    document.body.append(alertClone);
+    setTimeout(() => alertContainer.remove(), 3000);
+  } else {
     clearElement(parentElement);
     parentElement.appendChild(alertClone);
   }
