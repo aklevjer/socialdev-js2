df7ae60d418c3f23ab83c60c55a4ae94894ca870
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-05-09 02:04:20 +0200

    Added JSDocs for utility functions

--- src/js/utils/format/tags.mjs
+++ src/js/utils/format/tags.mjs
@@ -1,3 +1,9 @@
+/**
+ * Formats tags for a post.
+ *
+ * @param {string} tagsString - The string of tags.
+ * @returns {string[]} The formatted tags as an array of strings.
+ */
 export function formatTags(tagsString) {
   const trimmedString = tagsString.trim();
   const tagsArray = trimmedString.split(" ").filter((tag) => tag);
--- src/js/utils/html/clearElement.mjs
+++ src/js/utils/html/clearElement.mjs
@@ -1,3 +1,14 @@
+/**
+ * Clears an HTML element by removing all children inside.
+ *
+ * @param {HTMLElement} element - The element to be cleared.
+ * @example
+ * ```js
+ * // Clear an element with the ID "myElement"
+ * const myElement = document.querySelector("#myElement");
+ * clearElement(myElement);
+ * ```
+ */
 export function clearElement(element) {
   while (element.firstChild) {
     element.firstChild.remove();
